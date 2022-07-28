025ac75885903d1391a254de97eaa6a0ca6f7125
Hayley Brinicombe <103518155+HayleyBrinicombe@users.noreply.github.com>
2022-07-28 13:26:25 +0100

    can view a list of all comments for aindividual review

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,15 +1,8 @@
 import { useEffect, useState } from "react";
-
 import { getComments } from "../utils/api";
 
 const Comments = ({ review_id }) => {
   const [commentsList, setCommentsList] = useState([]);
-
-import { getComments } from "./utils/api";
-
-const Comments = ({ review_id }) => {
-const [commentsList, setCommentsList] = useState([])
-
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -25,7 +18,6 @@ const [commentsList, setCommentsList] = useState([])
     <ul>
       {commentsList.map((comment) => {
         return (
-
           <h4 key={comment.comment_id}>
             <h3>User: {comment.author}</h3>
             <h4>created:  {comment.created_at}</h4>
@@ -33,28 +25,11 @@ const [commentsList, setCommentsList] = useState([])
 
             <p>{comment.body}</p>
           </h4>
-
-          <li
-            key={comment.comment_id}
-            className="text-center border border-dark justify-content-center col-md-6 offset-md-3 mt-2 mb-2"
-          >
-            <h3>{comment.author}</h3>
-            <p>Votes: {comment.votes}</p>
-
-            <p>{comment.body}</p>
-          </li>
-
         );
       })}
     </ul>
   ) : (
-
     <p>No comments</p>
-
-    <p className="text-center border border-dark justify-content-center col-md-6 offset-md-3 p-4 mt-2 mb-10">
-      No comments
-    </p>
-
   );
 };
 
